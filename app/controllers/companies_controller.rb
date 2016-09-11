3e7d9c8d86ccb6e66d16b174160fda3426970c78
Greg Perlman <gperlman27@gmail.com>
2016-09-11 13:01:05 -0400

    add create action to companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -10,4 +10,18 @@ class CompaniesController < ApplicationController
   def new
     @company = Company.new
   end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :url, :username, :password, :description)
+  end
 end
