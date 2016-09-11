ffa53b6934d63f9f3a3fdbd1d858e26420613da2
Greg Perlman <gperlman27@gmail.com>
2016-09-11 13:16:18 -0400

    add edit action to companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -20,6 +20,20 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    
+    if @company.update(company_params)
+      redirect_to @company
+    else
+      render 'edit'
+    end
+  end
+
   private
   def company_params
     params.require(:company).permit(:name, :url, :username, :password, :description)
